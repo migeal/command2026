b844854a3755c8efa508eae77efd3bce478ea240
migeal <99619520+migeal@users.noreply.github.com>
2026-03-15 16:34:59 -0700

    changes to auto, added commands with no stop call

---
+++ src/main/java/frc/robot/commands/FireCon.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.Command;
+/** An example command that uses an example subsystem. */
+public class FireCon extends Command {
+  @SuppressWarnings("PMD.UnusedPrivateField")
+  private final Shooter m_shooter;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public FireCon(Shooter subsystem) {
+    m_shooter = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+     m_shooter.fire();
+    // System.out.println("Fire is called");
+  }
+   
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
